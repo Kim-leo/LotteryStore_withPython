9b2519d55e278854320a4a4cb920de19770af145
Kim - Leo <77371366+Kim-leo@users.noreply.github.com>
2022-05-26 23:57:39 +0900

    본인 복권번호 입력 및 당첨확인 함수 구현

--- lottoMap.py
+++ lottoMap.py
@@ -20,7 +20,8 @@ map에 핀을 찍어보도록 하겠습니다.
 4. 동행복권 사이트에서 당첨번호 7개와 몇회차인지 정보 크롤링하기
     4.1 당첨번호 7개를 배열에 저장
     4.2 N회차 + 날짜를 str으로 저장
-5. 랜덤번호 7개 생성 (1에서 45사이의 자연수)
+    4.3 당첨확인 함수 생성
+5. 랜덤번호 6개 생성 (1에서 45사이의 자연수)
 '''
 
 # 1. 카카오맵 API 가져오기
@@ -68,6 +69,10 @@ for i in range(46):
     
     
 # 4. 동행복권 사이트에서 당첨번호 7개와 몇회차인지 정보 크롤링하기
+winBox = []
+winBoxWithoutBonus = []
+date = ''
+
 def crawlURL():
     url = 'https://dhlottery.co.kr/common.do?method=main'
     response = requests.get(url)
@@ -75,26 +80,64 @@ def crawlURL():
     soup = BeautifulSoup(html, 'html.parser')
 
     # 4.1 당첨번호 7개를 배열에 저장
-    numBox = []
+    global winBox
+    global winBoxWithoutBonus
+    global date
     for i in range(1, 7):
-        nums = soup.find('span', {'id': 'drwtNo{}'.format(i)})
-        numBox.append(nums.text)
+        winNums = soup.find('span', {'id': 'drwtNo{}'.format(i)})
+        winBox.append(winNums.text)
+    winBoxWithoutBonus = set(map(int, winBox))
+    
     bonus = soup.find('span', {'id': 'bnusNo'})
-    numBox.append(bonus.text)
+    winBox.append(bonus.text)
+    winBox = set(map(int, winBox)) #numBox를 int요소가 있는 set으로 저장. (추후 당첨확인을 위함.)
 
     # 4.2 N회차 + 날짜를 str으로 저장
     date = soup.find('a', {'id': 'goByWin1'})
     date = date.text
     
-    print(numBox)
+    print(winBox)
     print(date)
+    print(winBoxWithoutBonus)
+crawlURL()
 
+# 4.3 당첨확인 함수 생성
+def checkMyLuck():
+    # 내 복권번호 6자리 입력하기
+    N = 6
+    myNumbers = []
+    for i in range(N):
+        myNumbers.append(int(input()))
     
-# 5. 랜덤번호 7개 생성 (1에서 45사이의 자연수)
+    myNumbers.sort()
+    myNumbers = set(myNumbers) #numBox와 본인의 번호를 비교를 위해 set으로 바꿈
+    interNum = winBox & myNumbers
+    howManyLucky = len(interNum) #등수를 확인하기 위해 얼마나 겹쳤는지 int로 확인
+    
+    # 1등 ~ 꽝 출력
+    if len(winBox & myNumbers) == 6:
+        if len(winBoxWithoutBonus & myNumbers) == 6:
+            print("1등")
+        else:
+            print("2등")
+    elif len(winBox & myNumbers) == 5:
+        print("3등")
+    elif len(winBox & myNumbers) == 4:
+        print("4등")
+    elif len(winBox & myNumbers) == 3:
+        print("5등")
+    else:
+        print("꽝~")
+    
+
+checkMyLuck()    
+    
+# 5. 랜덤번호 6개 생성 (1에서 45사이의 자연수)
 def randomNumbers():
-    randomNumsList = random.sample(range(1,46), 7)
-    bonusNum = random.choice(randomNumsList)
-    randomNumsList.remove(bonusNum)
+    randomNumsList = random.sample(range(1,46), 6)
     randomNumsList.sort()
     
-    print(randomNumsList, bonusNum)
+    print(randomNumsList)
+
+
+
